1ea52aaf9eca227cc7c29516b2370ac87d0a3b01
Kyle <kyle201817146@gmail.com>
2025-04-26 21:27:27 +0800

    Update CUILog and add CrashReporterClient.c support (#2)

--- CoreUI/CUILog.m
+++ CoreUI/CUILog.m
@@ -3,17 +3,41 @@
 //  CoreUI
 //
 //  Audited for macOS 15.0
-//  Status: WIP
+//  Status: Complete
 
 #import "CUILog.h"
+#import "CrashReporterClient.h"
 
-int32_t __logToStderr = 0;
+static BOOL __logToStderr = NO;
 
-static const char *lastErrorString = NULL;
+void CUILogLogToStderr(BOOL logToStderr) {
+    __logToStderr = logToStderr;
+}
+
+static BOOL __renditionLogginEnabled;
+
+void CUILogEnableRenditionLog(BOOL enabled) {
+    __renditionLogginEnabled = enabled;
+}
+
+BOOL CUILogRenditionLogEnabled(void) {
+    return __renditionLogginEnabled;
+}
+
+static os_log_t __handle;
+static dispatch_once_t __onceToken;
+
+CUI_INLINE
+static os_log_t _CUILogHandle(void) {
+    dispatch_once(&__onceToken, ^{
+        __handle = os_log_create("com.apple.coreui", "framework");
+    });
+    return __handle;
+}
 
 void _CUILog(CUILogLevel level, const char * _Nonnull message, ...) {
-    static BOOL _showDebugLogs = NO;
     static dispatch_once_t _onceDefaultToken;
+    static BOOL _showDebugLogs = NO;
     dispatch_once(&_onceDefaultToken, ^{
         _showDebugLogs = [NSUserDefaults.standardUserDefaults boolForKey:@"CUIShowDebugLogs"];
     });
@@ -21,56 +45,45 @@ void _CUILog(CUILogLevel level, const char * _Nonnull message, ...) {
         return;
     }
     @autoreleasepool {
-        va_list arguments;
-        va_start(arguments, message);
         if (__logToStderr) {
+            va_list arguments;
+            va_start(arguments, message);
             NSString *result = [[NSString alloc] initWithFormat:[[NSString alloc] initWithUTF8String:message] arguments:arguments];
+            va_end(arguments);
             switch (level) {
                 case CUILogLevelDefault:
                 case CUILogLevelInfo:
                 case CUILogLevelDebug:
-                    fprintf(stderr, "%s\n", [result UTF8String]);
+                    fprintf(stderr, "%s\n", result.UTF8String);
                     break;
                 case CUILogLevelError:
-                    fprintf(stderr, "%s\n", [result UTF8String]);
-                    lastErrorString = [result UTF8String];
+                    fprintf(stderr, "%s\n", result.UTF8String);
+                    CRSetCrashLogMessage(result.UTF8String);
                 default:
                     break;
             }
         } else {
+            va_list arguments;
+            va_start(arguments, message);
             NSString *result = [[NSString alloc] initWithFormat:[[NSString alloc] initWithUTF8String:message] arguments:arguments];
+            va_end(arguments);
             switch (level) {
                 case CUILogLevelDefault:
-                    os_log(_CUILogHandle(), "%{public}@", result);
+                    os_log_with_type(_CUILogHandle(), OS_LOG_TYPE_DEFAULT, "%{public}@", result);
                     break;
                 case CUILogLevelInfo:
-                    os_log_info(_CUILogHandle(), "%{public}@", result);
+                    os_log_with_type(_CUILogHandle(), OS_LOG_TYPE_INFO, "%{public}@", result);
                     break;
                 case CUILogLevelDebug:
-                    os_log_debug(_CUILogHandle(), "%{public}@", result);
+                    os_log_with_type(_CUILogHandle(), OS_LOG_TYPE_DEBUG, "%{public}@", result);
                     break;
                 case CUILogLevelError:
-                    os_log_error(_CUILogHandle(), "%{public}@", result);
+                    os_log_with_type(_CUILogHandle(), OS_LOG_TYPE_ERROR, "%{public}@", result);
                     os_log_fault(_CUILogHandle(), "%{public}@", result);
-                    lastErrorString = [result UTF8String];
-                    break;
+                    CRSetCrashLogMessage(result.UTF8String);
                 default:
                     break;
             }
         }
-        va_end(arguments);
     }
-}
-
-os_log_t _CUILogHandle(void) {
-    static os_log_t __handle;
-    static dispatch_once_t __onceToken;
-    dispatch_once(&__onceToken, ^{
-        __handle = os_log_create("com.apple.coreui", "framework");
-    });
-    return __handle;
-}
-
-void CUILogLogToStderr(int32_t verbose) {
-    __logToStderr = verbose;
 }
---
+++ CoreUI/CrashReporterClient.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2010 Apple Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+/***********************************************************************
+ * Not to be installed in /usr/local/include
+ ***********************************************************************/
+
+#ifndef _LIBC_CRASHREPORTERCLIENT_H
+#define _LIBC_CRASHREPORTERCLIENT_H
+
+#ifdef LIBC_NO_LIBCRASHREPORTERCLIENT
+
+/* Fake the CrashReporterClient API */
+#define CRGetCrashLogMessage() 0
+#define CRSetCrashLogMessage(x) /* nothing */
+#define CRSetCrashLogMessage2(x) /* nothing */
+
+#else /* !LIBC_NO_LIBCRASHREPORTERCLIENT */
+
+/* Include the real CrashReporterClient.h */
+#include <stdint.h>
+
+#define CRASHREPORTER_ANNOTATIONS_SECTION "__crash_info"
+#define CRASHREPORTER_ANNOTATIONS_VERSION 5
+#define CRASH_REPORTER_CLIENT_HIDDEN __attribute__((visibility("hidden")))
+
+#define _crc_make_getter(attr) ((const char *)(unsigned long)gCRAnnotations.attr)
+#define _crc_make_setter(attr, arg) (gCRAnnotations.attr = (uint64_t)(unsigned long)(arg))
+#define CRGetCrashLogMessage() _crc_make_getter(message)
+#define CRSetCrashLogMessage(m) _crc_make_setter(message, m)
+#define CRGetCrashLogMessage2() _crc_make_getter(message2)
+#define CRSetCrashLogMessage2(m) _crc_make_setter(message2, m)
+
+struct crashreporter_annotations_t {
+    uint64_t version;
+    uint64_t message;
+    uint64_t signature_string;
+    uint64_t backtrace;
+    uint64_t message2;
+    uint64_t thread;
+    uint64_t dialog_mode;
+    uint64_t abort_cause;
+};
+
+CRASH_REPORTER_CLIENT_HIDDEN
+extern struct crashreporter_annotations_t gCRAnnotations;
+
+#endif /* !LIBC_NO_LIBCRASHREPORTERCLIENT */
+
+#endif /* _LIBC_CRASHREPORTERCLIENT_H */
